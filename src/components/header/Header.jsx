c93d0332ee50084e795abb57b64b0ccabdd10c6a
DE-Nandan <nandankmrde@gmail.com>
2025-09-27 13:26:09 +0530

    headerUp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,85 +6,75 @@ import HeaderSocials from './HeaderSocials'
 import Typewriter from "typewriter-effect";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 const Header = () => {
-  useEffect(() => {
-    AOS.init({
-      // Global settings:
-      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-      initClassName: 'aos-init', // class applied after initialization
-      animatedClassName: 'aos-animate', // class applied on animation
-      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-      
-    
-      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-      offset: 120, // offset (in px) from the original trigger point
-      delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 1500, // values from 0 to 3000, with step 50ms
-      easing: 'ease', // default easing for AOS animations
-      once: false, // whether animation should happen only once - while scrolling down
-      mirror: false, // whether elements should animate out while scrolling past them
-      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-    
-    }
+  useEffect(() => {
+    AOS.init({
+      disable: false,
+      startEvent: 'DOMContentLoaded',
+      initClassName: 'aos-init',
+      animatedClassName: 'aos-animate',
+      useClassNames: false,
+      disableMutationObserver: false,
+      debounceDelay: 50,
+      throttleDelay: 99,
+      offset: 120,
+      delay: 0,
+      duration: 1500,
+      easing: 'ease',
+      once: false,
+      mirror: false,
+      anchorPlacement: 'top-bottom',
+    });
+    AOS.refresh();
+  }, []);
 
-    );
-    AOS.refresh();
-  }, []);
-  return (
-   <header>
-     <div className="container header__container">
-      <h5>Hello I'm</h5>
-      {/* <h1>Nandan Kumar</h1> */}
-      <div className="TypeW">
-      <Typewriter
-       
-       onInit={(typewriter)=> {
-  
-       typewriter
-        
-       .typeString("Nandan Kumar")
-         
-      //  .pauseFor(1000)
-      //  .deleteAll()
-      //  .typeString("Welcomes You")
-       .start();
-       }}
-       />
-       </div>
-      <div className="TypeW2">
-      <Typewriter
-       
-       onInit={(typewriter)=> {
-  
-       typewriter
-       .pauseFor(1000)  
+  return (
+    <header>
+      <div className="container header__container">
+        {/* Outdated page notification */}
+        <div style={{
+          backgroundColor: '#ffcc00',
+          color: '#333',
+          padding: '10px',
+          borderRadius: '5px',
+          textAlign: 'center',
+          marginBottom: '15px',
+          fontWeight: 'bold'
+        }}>
+          ⚠️ This page is outdated. Please check back later for updates.
+        </div>
 
-       .typeString("Former SDE intern @ Oracle | Web & Android Developer")
-         
-      
-      //  .deleteAll()
-      //  .typeString("Welcomes You")
-       .start();
-       }}
-       />
-       </div>
-     
-       
-      {/* <h5 className="text-light">Programmer | Web Developer</h5> */}
-      <CTA/>
-      <HeaderSocials/>
-      <div className="me">
-        <img src={ME} alt="me" className='lol' />
-      </div>
-     <a href='#contact' className='scroll__down'>Scroll Down</a>
+        <h5>Hello I'm</h5>
+        <div className="TypeW">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Nandan Kumar")
+                .start();
+            }}
+          />
+        </div>
+        <div className="TypeW2">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .pauseFor(1000)
+                .typeString("Former SDE intern @ Oracle | Web & Android Developer")
+                .start();
+            }}
+          />
+        </div>
 
-     </div>
-   </header>
-  )
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" className='lol' />
+        </div>
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
+  )
 }
 
 export default Header
